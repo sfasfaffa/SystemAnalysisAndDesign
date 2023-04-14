23b7e96624c39b88c5c38732ccf056e47a659ff8
‘PDY’ <1470340693@qq.com>
2023-04-14 22:27:00 +0800

    4.14 final update

--- src/main/java/com/example/system/controller/LecturerController.java
+++ src/main/java/com/example/system/controller/LecturerController.java
@@ -1,10 +1,25 @@
 package com.example.system.controller;
 
+import com.example.system.model.Result;
+import com.example.system.request.LecturerRequest;
+import com.example.system.service.LecturerService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("lecture")
+@RequestMapping("/lecture")
 public class LecturerController {
-
+    @Autowired
+    LecturerService lecturerService;
+    @PostMapping(value = "/create")
+    public Result create(@RequestBody LecturerRequest lecturerRequest){
+        return lecturerService.create(lecturerRequest);
+    }
+    @PostMapping(value = "/select/all")
+    public Result selectAll(){
+        return lecturerService.selectAll();
+    }
 }
--- src/main/java/com/example/system/service/TrainingCommentTableService.java
+++ src/main/java/com/example/system/service/TrainingCommentTableService.java
@@ -2,9 +2,12 @@ package com.example.system.service;
 
 import com.example.system.dao.TrainingCommentTableDao;
 import com.example.system.entity.TrainingCommentTable;
+import com.example.system.entity.User;
 import com.example.system.model.Result;
 import com.example.system.request.TrainingCommentTableRequest;
 import com.example.system.util.ResultUtil;
+import org.apache.shiro.SecurityUtils;
+import org.apache.shiro.subject.Subject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -19,5 +22,13 @@ public class TrainingCommentTableService {
         trainingCommentTable.setTrainingSatisfaction(trainingCommentTableRequest.getTrainingSatisfaction());
         trainingCommentTableDao.save(trainingCommentTable);
         return ResultUtil.success();
+    }
+    public Result delete(TrainingCommentTableRequest trainingCommentTableRequest){
+        TrainingCommentTable trainingCommentTable = trainingCommentTableDao.getOne(trainingCommentTableRequest.getId());
+        trainingCommentTableDao.delete(trainingCommentTable);
+        return ResultUtil.success();
+    }
+    public Result getAll(){
+        return ResultUtil.success(trainingCommentTableDao.findAll());
     }
 }
